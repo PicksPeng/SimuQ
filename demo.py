7cd74c95d0583d7eadc587c1b094782bfc06ca7f
Yuxiang Peng <pickspeng@gmail.com>
2022-12-08 21:03:20 -0500

    added README and rigetti in demo.py

--- demo.py
+++ demo.py
@@ -1,7 +1,7 @@
 import time
 from simuq.solver import generate_as
 
-K = 40
+K = 20
 #N = K * K
 N = K
 
@@ -11,6 +11,7 @@ D = 20
 
 
 qsys = 'MIS'
+
 if qsys == 'MIS' :
     from systems.mis import GenQS
     qs = GenQS('Chain', k = K, dis_num = D)
@@ -20,12 +21,10 @@ elif qsys == 'QAOA' :
     from systems.qaoa import GenQS
     qs = GenQS(n = K, p = D)
 
-# from systems.heis5 import qs
-# from systems.single_x import qs
-# from systems.annealing import qs
 
 
-mode = 'IBM OpenPulse'
+mode = 'Rigetti'
+
 if mode == 'Circuit' :
     # Circuit / Ion Trap Setting
     from aais.iontrap import GenMach
@@ -38,6 +37,8 @@ if mode == 'Circuit' :
         print(f'finishing {r}')
     end_time = time.time()
     print(f'Avg Time consumption: {(end_time - start_time) / Repetition} s')
+
+
 elif mode == 'Rydberg1d Bloqade' :
     # Bloqade Rydberg Setting
     from aais.rydberg1d import GenMach
@@ -50,6 +51,8 @@ elif mode == 'Rydberg1d Bloqade' :
         print(f'finishing {r}')
     end_time = time.time()
     print(f'Avg Time consumption: {(end_time - start_time) / Repetition} s')
+
+
 elif mode == 'Rydberg2d Bloqade' :
     # Bloqade Rydberg Setting
     from aais.rydberg2d import GenMach
@@ -62,6 +65,8 @@ elif mode == 'Rydberg2d Bloqade' :
         print(f'finishing {r}')
     end_time = time.time()
     print(f'Avg Time consumption: {(end_time - start_time) / Repetition} s')
+
+
 elif mode == 'Rydberg QuEra' :
     # Braket QuEra Rydberg Setting
     from aais.rydberg1d_aws import GenMach
@@ -69,6 +74,8 @@ elif mode == 'Rydberg QuEra' :
     n_mach = N
     mach = GenMach(n_mach)
     code = transpile(*generate_as(qs, mach, 1))
+
+
 elif mode == 'IBM OpenPulse' :
     # IBM Qiskit OpenPulse Setting
     from qiskit.providers.fake_provider import FakeJakarta, FakeGuadalupe, FakeToronto, FakeWashington
@@ -116,32 +123,15 @@ elif mode == 'IBM OpenPulse' :
     
     end_time = time.time()
     print(f'Avg Time consumption: {(end_time - start_time) / 5} s')
-    
-    
-
-# IBM Qiskit 5qubit Setting
-'''
-from aais.ibm5 import mach
-from backends.qiskit_5 import transpile
-circ=transpile(*generate_as(qs, mach, 1, 0.5))
-'''
 
 
-
-# generate_as(FloquetQS, FluxSCMach)
-# generate_as(qs, mach, 1)
-
-
-# from aais.rydberg2d import Rydberg as mach
-# from backends.bloqade_rydberg2d import transpile
-
-
-#assembly=generate_as(qs, mach, 1)
-
-"""
-circ=transpile(*generate_as(qs, mach, 1, 0.5))
-import pickle
-
-with open('circ.pickle', 'wb') as f:
-    pickle.dump(circ, f)
-"""
+elif mode == 'Rigetti' :
+    from aais.rigetti import get_mach
+    mach = get_mach()
+    assembly = generate_as(qs, mach, 1)
+    with open("qaoa_rigetti.as","w+") as f:
+        f.write(str(assembly[1])+"\n")
+        for item in assembly[2]:
+            f.write(str(item)+"\n")
+        for item in assembly[3]:
+            f.write(str(item)+"\n")

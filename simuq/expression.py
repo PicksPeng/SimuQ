1903115de5b3cd13a579fd9485dd3cbdaec7a922
Cedric Lin <67704428+licedric@users.noreply.github.com>
2023-08-26 14:13:16 -0700

    Refactor expressions to store variables. (#7)

--- simuq/expression.py
+++ simuq/expression.py
@@ -20,17 +20,23 @@ import numpy as np
 class BaseVar :
     """ The basic variables.
 
-    The constrainsts specific to the variables are stored here,
+    The constraints specific to the variables are stored here,
     like the initial value, lower and upper bounds of them.
     """
-    def __init__(self, mach) :
-        self.mach = mach
-        self.init_value = 0
-        self.lower_bound = -np.inf
-        self.upper_bound = np.inf
+    def __init__(self, init_value=0, lower_bound=-np.inf, upper_bound=np.inf):
+        self.init_value = init_value
+        self.lower_bound = lower_bound
+        self.upper_bound = upper_bound
+        self.index = None
 
     def to_exp(self) :
-        pass
+        return Expression.id_var(self)
+
+    # Index should allow the value of the BaseVar to be obtained from a list of values of global and local variables.
+    # Global variables have indices from 0 to num_gvars - 1
+    # Local variables have indices from num_gvars to num_gvars + num_lvars - 1
+    def set_index(self, index):
+        self.index = index
 
     def __neg__(self) :
         return -self.to_exp()
@@ -47,30 +53,33 @@ class BaseVar :
     def __truediv__(self, other) :
         return self.to_exp() / other
 
+# Given two lists, return the union, and the indices of the items of the original lists in the union.
+def find_union_indices(list1, list2):
+    union_list = list(set(list1).union(list2))
+    union_dict = dict((item, index) for index, item in enumerate(union_list))
+    indices1 = [union_dict[item] for item in list1]
+    indices2 = [union_dict[item] for item in list2]
+    return union_list, indices1, indices2
+
 class Expression :
     """ The expressions.
     
     It is effectively a function taking a valuation of global variables
     and local variables and generating a number.
     """
-    def __init__(self, mach, exp) :
-        self.mach = mach
+    def __init__(self, exp, vars) :
         self.exp = exp
+        self.vars = vars
 
     @classmethod
-    def unit(cls, mach) :
-        exp = lambda gvars, lvars : 1
-        return cls(mach, exp)
-    
-    @classmethod
-    def id_gvar(cls, mach, index) :
-        exp = lambda gvars, lvars : gvars[index]
-        return cls(mach, exp)
+    def unit(cls) :
+        exp = lambda values: 1
+        return cls(exp, [])
 
     @classmethod
-    def id_lvar(cls, mach, index) :
-        exp = lambda gvars, lvars : lvars[index]
-        return cls(mach, exp)
+    def id_var(cls, var) :
+        exp = lambda values: values[0]
+        return cls(exp, [var])
 
     @classmethod
     def cos(cls, e) :
@@ -81,8 +90,8 @@ class Expression :
         if isinstance(e, np.float64) :
             return np.cos(e)
 
-        def exp(gvars, lvars) :
-            sub = e.exp(gvars, lvars)
+        def exp(values) :
+            sub = e.exp(values)
             if type(sub) == int  or  type(sub) == float :
                 return math.cos(sub)
             if isinstance(sub, np.float64) :
@@ -90,10 +99,9 @@ class Expression :
             import dreal as dr
             if isinstance(sub, dr._dreal_py.Variable)  or  isinstance(sub, dr._dreal_py.Expression) :
                 return dr.cos(sub)
-            pp = qq
             return NotImplemented
 
-        return cls(e.mach, exp)
+        return cls(exp, e.vars)
 
     @classmethod
     def sin(cls, e) :
@@ -104,36 +112,38 @@ class Expression :
         if isinstance(e, np.float64) :
             return np.sin(e)
         
-        def exp(gvars, lvars) :
-            sub = e.exp(gvars, lvars)
+        def exp(values) :
+            sub = e.exp(values)
             if type(sub) == int  or  type(sub) == float :
                 return math.sin(sub)
             if isinstance(sub, np.float64) :
                 return np.sin(sub)
             import dreal as dr
-            if type(sub) == dr._dreal_py.Variable  or  type(sub) == d._dreal_py.Expression :
+            if type(sub) == dr._dreal_py.Variable  or  type(sub) == dr._dreal_py.Expression :
                 return dr.sin(sub)
-            pp = qq
             return NotImplemented
         
-        return cls(e.mach, exp)
+        return cls(exp, e.vars)
 
     def __neg__(self) :
-        exp = lambda gvars, lvars : -self.exp(gvars, lvars)
-        e = Expression(self.mach, exp)
+        exp = lambda vars : -self.exp(vars)
+        e = Expression(exp, self.vars)
         return e
 
     def __add__(self, other) :
         if type(other) == int  or  type(other) == float  or  type(other) == complex :
-            exp = lambda gvars, lvars : self.exp(gvars, lvars) + other
-            e = Expression(self.mach, exp)
+            exp = lambda values : self.exp(values) + other
+            e = Expression(exp, self.vars)
             return e
         if isinstance(other, BaseVar) :
             other = other.to_exp()
         if not hasattr(other, "exp") :
             return NotImplemented
-        exp = lambda gvars, lvars : self.exp(gvars, lvars) + other.exp(gvars, lvars)
-        e = Expression(self.mach, exp)
+
+        new_vars, indices1, indices2 = find_union_indices(self.vars, other.vars)
+        exp = lambda values: self.exp([values[index] for index in indices1]) + other.exp(
+            [values[index] for index in indices2])
+        e = Expression(exp, new_vars)
         return e
 
     def __sub__(self, other) :
@@ -150,15 +160,18 @@ class Expression :
 
     def __mul__(self, other) :
         if type(other) == int  or  type(other) == float  or  type(other) == complex :
-            exp = lambda gvars, lvars : self.exp(gvars, lvars) * other
-            e = Expression(self.mach, exp)
+            exp = lambda values : self.exp(values) * other
+            e = Expression(exp, self.vars)
             return e
         if isinstance(other, BaseVar) :
             other = other.to_exp()
         if not hasattr(other, "exp") :
             return NotImplemented
-        exp = lambda gvars, lvars : self.exp(gvars, lvars) * other.exp(gvars, lvars)
-        e = Expression(self.mach, exp)
+
+        new_vars, indices1, indices2 = find_union_indices(self.vars, other.vars)
+        exp = lambda values: self.exp([values[index] for index in indices1]) * other.exp(
+            [values[index] for index in indices2])
+        e = Expression(exp, new_vars)
         return e
 
     def __rmul__(self, other) :
@@ -172,42 +185,54 @@ class Expression :
 
     def __pow__(self, other) :
         if type(other) == int  or  type(other) == float  or  type(other) == complex :
-            exp = lambda gvars, lvars : self.exp(gvars, lvars) ** other
-            e = Expression(self.mach, exp)
+            exp = lambda values : self.exp(values) ** other
+            e = Expression(exp, self.vars)
             return e
         if isinstance(other, BaseVar) :
             other = other.to_exp()
         if not hasattr(other, "exp") :
             return NotImplemented
-        exp = lambda gvars, lvars : self.exp(gvars, lvars) ** other.exp(gvars, lvars)
-        e = Expression(self.mach, exp)
+
+        new_vars, indices1, indices2 = find_union_indices(self.vars, other.vars)
+        exp = lambda values: self.exp([values[index] for index in indices1]) ** other.exp(
+            [values[index] for index in indices2])
+        e = Expression(exp, new_vars)
         return e
 
     def __truediv__(self, other) :
         if type(other) == int  or  type(other) == float  or  type(other) == complex :
-            exp = lambda gvars, lvars : self.exp(gvars, lvars) / other
-            e = Expression(self.mach, exp)
+            exp = lambda values : self.exp(values) / other
+            e = Expression(exp, self.vars)
             return e
         if isinstance(other, BaseVar) :
             other = other.to_exp()
         if not hasattr(other, "exp") :
             return NotImplemented
-        exp = lambda gvars, lvars : self.exp(gvars, lvars) / other.exp(gvars, lvars)
-        e = Expression(self.mach, exp)
+        new_vars, indices1, indices2 = find_union_indices(self.vars, other.vars)
+        exp = lambda values: self.exp([values[index] for index in indices1]) / other.exp(
+            [values[index] for index in indices2])
+        e = Expression(exp, new_vars)
         return e
 
     def __rtruediv__(self, other) :
         if type(other) == int  or  type(other) == float  or  type(other) == complex :
-            exp = lambda gvars, lvars : other / self.exp(gvars, lvars)
-            e = Expression(self.mach, exp)
+            exp = lambda values : other / self.exp(values)
+            e = Expression(exp, self.vars)
             return e
         if isinstance(other, BaseVar) :
             other = other.to_exp()
         if not hasattr(other, "exp") :
             return NotImplemented
-        exp = lambda gvars, lvars : other.exp(gvars, lvars) / self.exp(gvars, lvars)
-        e = Expression(self.mach, exp)
+
+        new_vars, indices1, indices2 = find_union_indices(other.vars, self.vars)
+        exp = lambda values: other.exp([values[index] for index in indices1]) / self.exp(
+            [values[index] for index in indices2])
+        e = Expression(exp, new_vars)
+
         return e
 
     def exp_eval(self, gvars, lvars) :
-        return self.exp(gvars, lvars)
+        # The weird comprehension below is to handle np array shapes
+        values = [gvar for gvar in gvars] + [lvar for lvar in lvars]
+        return_val = self.exp([values[var.index] for var in self.vars])
+        return return_val

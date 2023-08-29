bug fix: convert from radians to turns

simuq/backends/ionq_qiskit_native.py
@@ -58,7 +58,7 @@ def clean_as(n, boxes, edges):
                             circ.append(GPI2Gate(((accum_phase[q] + 3 * np.pi / 2) / (2 * np.pi)) % 1, [q]))
                             accum_phase[q] -= rot
                             accum_phase[q] %= (2 * np.pi)
-                            circ.append(GPI2Gate(((accum_phase[q] + np.pi / 2) % (2 * np.pi)) % 1, [q]))
+                            circ.append(GPI2Gate(((accum_phase[q] + np.pi / 2) / (2 * np.pi)) % 1, [q]))
                         # Rz(q, -phi)
                         accum_phase[q] += phi
                         accum_phase[q] %= (2 * np.pi)
@@ -100,8 +100,8 @@ def clean_as(n, boxes, edges):
                     # R_ZZ(theta)
                     if abs(theta) > 1e-5:
                         # R_X(-pi/2)
-                        circ.append(GPI2Gate((accum_phase[q0] + np.pi) % (2 * np.pi), [q0]))
-                        circ.append(GPI2Gate((accum_phase[q1] + np.pi) % (2 * np.pi), [q1]))
+                        circ.append(GPI2Gate(((accum_phase[q0] + np.pi) / (2 * np.pi)) % 1, [q0]))
+                        circ.append(GPI2Gate(((accum_phase[q1] + np.pi) / (2 * np.pi)) % 1, [q1]))
                         # R_YY(theta)
                         if (theta / (2 * np.pi)) % 1 <= 0.25 or (theta / (2 * np.pi)) % 1 >= 0.75:
                             circ.append(MSGate((accum_phase[q0] / (2 * np.pi) + 0.25) % 1, (accum_phase[q1] / (2 * np.pi) + 0.25) % 1, (theta / (2 * np.pi)) % 1, [q0, q1]))
@@ -114,8 +114,8 @@ def clean_as(n, boxes, edges):
                         else:
                             raise ValueError(f"Rotation angle is {theta}, should be between 0 and 2*pi")
                         # R_X(pi/2)
-                        circ.append(GPI2Gate((accum_phase[q0]) % (2 * np.pi), [q0]))
-                        circ.append(GPI2Gate((accum_phase[q1]) % (2 * np.pi), [q1]))
+                        circ.append(GPI2Gate(((accum_phase[q0]) / (2 * np.pi)) % 1, [q0]))
+                        circ.append(GPI2Gate(((accum_phase[q1]) / (2 * np.pi)) % 1, [q1]))
     return circ, accum_phase
 
 
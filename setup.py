d14455ae6da0866c9b732a44af30219914f29603
Cody Wang <speller26@gmail.com>
2023-09-18 10:23:38 -0700

    Add more details to setup.py (#27)

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
 from setuptools import find_namespace_packages, setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 with open("src/simuq/_version.py") as f:
     version = f.readlines()[-1].split()[-1].strip("\"'")
 
 setup(
-    name="SimuQ",
+    name="simuq",
     version=version,
     description="A Python package for quantum simulation with analog compilation",
-    url="https://github.com/PicksPeng/SimuQ",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://pickspeng.github.io/SimuQ/",
     license="BSD 3-Clause Clear",
     packages=find_namespace_packages(where="src"),
     package_dir={"": "src"},
@@ -35,4 +40,14 @@ setup(
         "all": ["simuq[braket, ionq, qiskit, qutip, dev]"],
         "dev": ["tox"],
     },
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Science/Research",
+        "Natural Language :: English",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
 )

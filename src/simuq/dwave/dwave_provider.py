2d11c1eccf2705b8c633f40ee997b86f18835cdd
jiaqileng <>
2023-12-04 20:21:56 -0800

    update run for ising

--- src/simuq/dwave/dwave_provider.py
+++ src/simuq/dwave/dwave_provider.py
@@ -11,31 +11,45 @@ from simuq.dwave.dwave_transpiler import DwaveTranspiler
 
 
 class DWaveProvider(BaseProvider):
-    def __init__(self, api_key, numruns=100, chain_strength_ratio=1.1):
+    def __init__(self, api_key, numruns=100, chain_strength):
         # insert all log in details
         super().__init__()
         self._samples = None
         self.api_key = api_key
         self.numruns = numruns
-        self.chain_strength_ratio = chain_strength_ratio
+        self.chain_strength = chain_strength
 
     def compile(self,
                 qs,
+                anneal_schedule,
                 tol=0.01,
                 trotter_num=6,
                 verbose=0):
         h = [0 for _ in range(qs.num_sites)]
-        J = {(i, j): 0 for i in range(qs.num_sites) for j in range(i + 1, qs.num_sites)}
+        #J = {(i, j): 0 for i in range(qs.num_sites) for j in range(i + 1, qs.num_sites)}
+        J = {}
         for ham in qs.evos[0][0].ham:
             keys = list(ham[0].d.keys())
             vals = list(ham[0].d.values())
             if 'Z' in vals:
                 if len(vals) == 1:
                     h[keys[0]] = ham[1]
-                elif len(vals) == 2:
+                elif len(vals) == 2 and ham[1] != 0:
                     J[(keys[0], keys[1])] = ham[1]
 
-        self.prog = h, J, None
+        self.prog = h, J, anneal_schedule
+
+    def run(self):
+        if self.prog is None:
+            raise Exception("No compiled job in record.")
+        qpu = DWaveSampler(token=self.api_key)
+        sampler = EmbeddingComposite(qpu)
+        h, J, anneal_schedule = self.prog
+        response = sampler.sample_ising(h, J, 
+                                       chain_strength=self.chain_strength,
+                                       num_reads=self.numruns,
+                                       anneal_schedule=anneal_schedule)
+        self.samples = list(response.samples())
 
     def isingToqubo(self, h, J):
         n = len(h)
@@ -57,7 +71,7 @@ class DWaveProvider(BaseProvider):
 
         return QUBO
 
-    def run(self):
+    def run_qubo(self):
         if self.prog is None:
             raise Exception("No compiled job in record.")
         qpu = DWaveSampler(token=self.api_key)
@@ -66,9 +80,10 @@ class DWaveProvider(BaseProvider):
         h = {i: h[i] for i in range(len(h))}
         qubo = self.isingToqubo(h, J)
         max_interaction_qhd = np.max(np.abs(np.array(list(qubo.values()))))
-        response = sampler.sample_qubo(qubo, 
-                                       chain_strength = self.chain_strength_ratio * max_interaction_qhd,
-                                       num_reads=self.numruns)
+        response = sampler.sample_qubo(qubo,
+                                       num_reads=self.numruns,
+                                       anneal_schedule=anneal_schedule,
+                                       anneal_schedule=1.1*max_interaction_qhd)
         self.samples = list(response.samples())
 
     def results(self):

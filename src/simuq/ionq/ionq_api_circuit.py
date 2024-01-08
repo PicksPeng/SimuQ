466afb873b268a4cf1f8e9a9f346da6a401d1ea7
Yuxiang Peng <pickspeng@gmail.com>
2024-01-08 00:03:41 -0500

    added gates for ionq.

--- src/simuq/ionq/ionq_api_circuit.py
+++ src/simuq/ionq/ionq_api_circuit.py
@@ -137,3 +137,27 @@ class IonQAPICircuit(IonQCircuit):
         circ = IonQAPICircuit(len(self._accum_phases))
         circ.add(self)
         return circ
+
+    def to_matrix(self):
+        from qiskit import QuantumCircuit
+        from qiskit.quantum_info import Operator
+
+        n = self.job["input"]["qubits"]
+        circ = QuantumCircuit(n)
+
+        from qiskit.circuit.library import RZGate
+        from qiskit_ionq import GPI2Gate, GPIGate, IonQProvider, MSGate
+
+        for ind, gate in enumerate(self.job["input"]["circuit"]):
+            if gate["gate"] == "gpi":
+                circ.append(GPIGate(gate["phase"]), [n - 1 - gate["target"]])
+            elif gate["gate"] == "gpi2":
+                circ.append(GPI2Gate(gate["phase"]), [n - 1 - gate["target"]])
+            elif gate["gate"] == "ms":
+                circ.append(MSGate(gate["phases"][1], gate["phases"][0], gate["angle"]), list(n - 1 - np.array(gate["targets"])))
+            else:
+                raise Exception("Unknown gate:", gate["gate"])
+        for q in range(self.job["input"]["qubits"]):
+            circ.append(RZGate(-self._accum_phases[q]), [n - 1 - q])
+
+        return Operator(circ).to_matrix()
